877cbb37e9a457c5b8c319346cdcc56e2422920e
Maxmillan <maxmillan.kuya@student.moringaschool.com>
2021-05-05 18:40:23 +0300

    add changes

--- script.js
+++ script.js
@@ -3,17 +3,16 @@ const shedA = 510
 const shedB = 308
 const shedC = 486
 const shedD = 572
-var liters per shade
-var items = document.
+var liters =pershade;
+var items = document
+
 var leapyear=[
     month=["january","february","march","april","may","june","july","agust","september","october","november","december"]
-    days=[31,29,31,30,31,30,31,31,30,31,30,31]
+    days =[31,29,31,30,31,30,31,31,30,31,30,31]
     for (i=0 ;i<monlength; i++){
         var sum=day[i] *days;
         document.getElementById('monthly')= 'Your income for ' + mon[i] +  ' is  Ksh' + sum;
         function displayDetails(){
             let total = totalProduction();
-            let price = 45;
-    }
-]
-
+            let price = 45
+        }
